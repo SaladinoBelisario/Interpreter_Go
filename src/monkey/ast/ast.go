9aead6f6504fa8187e24bd41e2a24f0d415636c8
Saladino Belisario <SaladinoBelisario@protonmail.ch>
2021-04-21 13:58:12 -0500

    Parsing statements

--- src/monkey/ast/ast.go
+++ src/monkey/ast/ast.go
@@ -1,5 +1,7 @@
 package ast
 
+import "monkey/token"
+
 type Node interface {
 	TokenLiteral() string
 }
@@ -18,6 +20,17 @@ type Program struct {
 	Statements []Statement
 }
 
+type LetStatement struct {
+	Token token.Token // the token.LET token
+	Name *Identifier
+	Value Expression
+}
+
+type Identifier struct {
+	Token token.Token // the token.IDENT token
+	Value string
+}
+
 func (p *Program) TokenLiteral() string {
 	if len(p.Statements) > 0 {
 		return p.Statements[0].TokenLiteral()
@@ -25,3 +38,8 @@ func (p *Program) TokenLiteral() string {
 		return ""
 	}
 }
+
+func (ls *LetStatement) statementNode(){}
+func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
+func (i *Identifier) expressionNode(){}
+func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
