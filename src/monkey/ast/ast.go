b9a1f742d9fba9b3c05796fcd558fd74138c9cc0
Saladino Belisario <SaladinoBelisario@protonmail.ch>
2021-04-21 14:45:33 -0500

    Pratt's parser

--- src/monkey/ast/ast.go
+++ src/monkey/ast/ast.go
@@ -1,9 +1,13 @@
 package ast
 
-import "monkey/token"
+import (
+	"bytes"
+	"monkey/token"
+)
 
 type Node interface {
 	TokenLiteral() string
+	String() string
 }
 
 type Statement interface {
@@ -31,6 +35,16 @@ type Identifier struct {
 	Value string
 }
 
+type ReturnStatement struct {
+	Token token.Token
+	ReturnValue Expression
+}
+
+type ExpressionStatement struct {
+	Token token.Token // The first token of the expression
+	Expression Expression
+}
+
 func (p *Program) TokenLiteral() string {
 	if len(p.Statements) > 0 {
 		return p.Statements[0].TokenLiteral()
@@ -43,3 +57,44 @@ func (ls *LetStatement) statementNode(){}
 func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
 func (i *Identifier) expressionNode(){}
 func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
+func (rs *ReturnStatement) statementNode() {}
+func (rs *ReturnStatement) TokenLiteral() string { return rs.Token.Literal }
+func (es *ExpressionStatement) statementNode() {}
+func (es *ExpressionStatement) TokenLiteral() string { return es.Token.Literal }
+func (i *Identifier) String() string { return i.Value }
+
+func (p *Program) String() string {
+	var out bytes.Buffer
+	for _, s := range p.Statements {
+		out.WriteString(s.String())
+	}
+	return out.String()
+}
+
+func (ls *LetStatement) String() string {
+	var out bytes.Buffer
+	out.WriteString(ls.TokenLiteral() + " ")
+	out.WriteString(ls.Name.String())
+	out.WriteString(" = ")
+	if ls.Value != nil {
+		out.WriteString(ls.Value.String())
+	}
+	out.WriteString(";")
+	return out.String()
+}
+
+func (rs *ReturnStatement) String() string {
+	var out bytes.Buffer
+	out.WriteString(rs.TokenLiteral() + " ")
+	if rs.ReturnValue != nil {
+		out.WriteString(rs.ReturnValue.String())
+	}
+	out.WriteString(";")
+	return out.String()
+}
+func (es *ExpressionStatement) String() string {
+	if es.Expression != nil {
+		return es.Expression.String()
+	}
+	return ""
+}
--- src/monkey/parser/parser_test.go
+++ src/monkey/parser/parser_test.go
@@ -7,13 +7,30 @@ import (
 )
 func TestLetStatements(t *testing.T) {
 	input := `
-let x = 5; let y = 10;
-let foobar = 838383; 
+return 5; 
+return 10;
+return 993322; 
 `
 	l := lexer.New(input)
 	p := New(l)
 	program := p.ParseProgram()
 	checkParserErrors(t, p)
+
+	if len(program.Statements) != 3 {
+		t.Fatalf("program.Statements does not contain 3 statements. got=%d",
+		len(program.Statements))
+	}
+	for _, stmt := range program.Statements {
+		returnStmt, ok := stmt.(*ast.ReturnStatement)
+		if !ok {
+			t.Errorf("stmt not *ast.returnStatement. got=%T", stmt)
+			continue
+		}
+		if returnStmt.TokenLiteral() != "return" {
+			t.Errorf("returnStmt.TokenLiteral not 'return', got %q", returnStmt.TokenLiteral())
+		}
+	}
+
 	if program == nil {
 		t.Fatalf("ParseProgram() returned nil")
 	}
