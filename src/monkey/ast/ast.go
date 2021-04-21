22d7654cd3742881d8202b4c2c88f8f903d8da8e
Saladino Belisario <SaladinoBelisario@protonmail.ch>
2021-04-21 15:22:18 -0500

    Integer Literals

--- src/monkey/ast/ast.go
+++ src/monkey/ast/ast.go
@@ -45,6 +45,11 @@ type ExpressionStatement struct {
 	Expression Expression
 }
 
+type IntegerLiteral struct {
+	Token token.Token
+	Value int64
+}
+
 func (p *Program) TokenLiteral() string {
 	if len(p.Statements) > 0 {
 		return p.Statements[0].TokenLiteral()
@@ -62,6 +67,9 @@ func (rs *ReturnStatement) TokenLiteral() string { return rs.Token.Literal }
 func (es *ExpressionStatement) statementNode() {}
 func (es *ExpressionStatement) TokenLiteral() string { return es.Token.Literal }
 func (i *Identifier) String() string { return i.Value }
+func (il *IntegerLiteral) expressionNode() {}
+func (il *IntegerLiteral) TokenLiteral() string { return il.Token.Literal }
+func (il *IntegerLiteral) String() string { return il.Token.Literal }
 
 func (p *Program) String() string {
 	var out bytes.Buffer
