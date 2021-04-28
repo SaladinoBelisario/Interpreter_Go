5c8bf5ece3fa7460e1391dfb2ff662ed88250056
Saladino Belisario <SaladinoBelisario@protonmail.ch>
2021-04-27 22:20:11 -0500

    Evaluator

--- src/monkey/object/object.go
+++ src/monkey/object/object.go
@@ -1,12 +1,21 @@
 package object
 
-import "fmt"
+import (
+	"bytes"
+	"fmt"
+	"monkey/ast"
+	"strings"
+)
 
 type ObjectType string
+
 const (
+	NULL_OBJ  = "NULL"
+	ERROR_OBJ = "ERROR"
 	INTEGER_OBJ = "INTEGER"
 	BOOLEAN_OBJ = "BOOLEAN"
-	NULL_OBJ = "NULL"
+	RETURN_VALUE_OBJ = "RETURN_VALUE"
+	FUNCTION_OBJ = "FUNCTION"
 )
 
 type Object interface {
@@ -24,9 +33,45 @@ type Boolean struct {
 
 type Null struct{}
 
-func (i *Integer) Inspect() string { return fmt.Sprintf("%d", i.Value) }
-func (i *Integer) Type() ObjectType { return INTEGER_OBJ }
+type ReturnValue struct {
+	Value Object
+}
+
+type Error struct {
+	Message string
+}
+
+type Function struct {
+	Parameters []*ast.Identifier
+	Body       *ast.BlockStatement
+	Env        *Environment
+}
+
+func (f *Function) Type() ObjectType { return FUNCTION_OBJ }
 func (b *Boolean) Type() ObjectType { return BOOLEAN_OBJ }
-func (b *Boolean) Inspect() string { return fmt.Sprintf("%t", b.Value) }
+func (b *Boolean) Inspect() string  { return fmt.Sprintf("%t", b.Value) }
 func (n *Null) Type() ObjectType { return NULL_OBJ }
-func (n *Null) Inspect() string { return "null" }
+func (n *Null) Inspect() string  { return "null" }
+func (rv *ReturnValue) Type() ObjectType { return RETURN_VALUE_OBJ }
+func (rv *ReturnValue) Inspect() string  { return rv.Value.Inspect() }
+func (e *Error) Type() ObjectType { return ERROR_OBJ }
+func (e *Error) Inspect() string  { return "ERROR: " + e.Message }
+func (i *Integer) Type() ObjectType { return INTEGER_OBJ }
+func (i *Integer) Inspect() string  { return fmt.Sprintf("%d", i.Value) }
+func (f *Function) Inspect() string {
+	var out bytes.Buffer
+
+	params := []string{}
+	for _, p := range f.Parameters {
+		params = append(params, p.String())
+	}
+
+	out.WriteString("fn")
+	out.WriteString("(")
+	out.WriteString(strings.Join(params, ", "))
+	out.WriteString(") {\n")
+	out.WriteString(f.Body.String())
+	out.WriteString("\n}")
+
+	return out.String()
+}
